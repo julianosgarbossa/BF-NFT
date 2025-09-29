3b480d9af698e20ce168618111da8566e198cf7b
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2025-09-29 16:24:42 -0300

    Criação do Projeto BF NFT

---
+++ BFNFT/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BFNFT
+//
+//  Created by Juliano Sgarbossa on 29/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
